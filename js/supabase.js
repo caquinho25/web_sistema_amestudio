6895974951302f30ba1b8dc5c01e8a50e4d1893a
caquinho25 <amesgiancarlo@gmail.com>
2026-03-19 23:50:05 -0500

    logo y supabase

---
+++ js/supabase.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://tedvahwmmztxwaxxxbio.supabase.co";
+const SUPABASE_KEY = "sb_publishable_YVf0vEOda8HoHmIYIYhL9Q_jFElta9t";
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
